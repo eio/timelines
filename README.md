5bb41093d75dc2fd9c304588d5200644de9801a2
eio <>
2019-10-18 19:21:29 +0200

    add images; set default to helix

--- README.md
+++ README.md
@@ -13,7 +13,11 @@ Try it out here: https://eio.github.io/gnss-ro-timeline/
 - https://spire.com/spirepedia/commercial-weather-data-pilot
 - http://www.geooptics.com/wordpress/wp-content/uploads/2016/04/AER_Severe_Weather_Study.pdf
 - https://www.sciencemag.org/news/2016/09/noaa-issues-first-contracts-private-weather-satellites
-
+- https://mars.nasa.gov/resources/7303/mariner-6-and-7-spacecraft-nearing-planet-mars/
+- https://www.gps.gov/systems/gps/control/
+- https://en.wikipedia.org/wiki/Global_Positioning_System
+- https://www.ion.org/museum/item_view.cfm?cid=7&scid=9&iid=22
+- https://www.cosmic.ucar.edu/
 
 ## Running Locally ##
 
--- js/setup.js
+++ js/setup.js
@@ -1,42 +1,12 @@
+export { init, animate };
+
 import * as THREE from './lib/three.module.js';
 import { TWEEN } from './lib/jsm/tween.module.min.js';
 import { TrackballControls } from './lib/jsm/TrackballControls.js';
 import { CSS3DRenderer, CSS3DObject } from './lib/jsm/CSS3DRenderer.js';
 
-export { init, animate };
-
-var table = [
-	"2020", "[event]", "",
-	1, 4,
-	"2020", "[event]", "",
-	2, 4,
-	"2019", "[event]", "",
-	3, 4,
-	"2019", "Spire continues to grow its constellation of small satellites, each equipped with their own GNSS Radio Occultation payload. Adding to what is already one of the largest private constellations in the world, the company exceeds 100 successfully deployed satellites early in the year.", "",
-	4, 4,
-	"2018-2017", "The National Environmental Satellite, Data, and Information Service (NESDIS) awards contracts to 3 satellite companies for Round Two of the CWDP. NOAA plans to review space-based RO data from Spire, GeoOptics, and PlanetIQ in order to determine its quality and potential value to NOAA’s weather forecasts and warnings.", "",
-	5, 4,
-	"2017-2016", "NOAA begins its inaugural Commercial Weather Data Pilot (CWDP). Round One culminates in the first ever contracts for private weather satellites being issued to Spire Global and GeoOptics.", "",
-	6, 4,
-	"2016", "Spire opens a second U.S campus in Boulder, Colorado. The company hires Dave Ector (former program manager for NASA’s COSMIC satellites) and Alexander MacDonald (former director of NOAA’s Earth System Research Laboratory), and starts drawing on the resources of the local weather ecosystem.", "",
-	7, 4,
-	"2012", "Spire Global - originally called NanoSatisfi Inc. - is founded in San Francisco, California. It begins as part of ArduSat, a project aiming to \"democratize access to space\".", "",
-	8, 4,
-	"2007", "The U.S. National Oceanic and Atmospheric Administration (NOAA) begins using the Cosmic constellation's radio occultation data in their operational forecasts.", "",
-	9, 4,
-	"2002-1999", "NASA follows the GPS/MET success with experiments on 6 international partnership missions, refining the systems and techniques of GPS sounding and demonstrating the robust low-cost technology. Several of these satellites continue to provide high-quality GPS remote sensing data today.", "",
-	10, 4,
-	"1998", "The University Corporation for Atmospheric Research partners with Taiwan's National Space Organization to take GPS remote sensing to the operational level with Cosmic - the first operational GPS occultation constellation - which is known as FORMOSAT-3 in Taiwan.", "",
-	11, 4,
-	"1995", "GPS/MET, proposed by the University Corporation for Atmospheric Research and sponsored by the U.S. National Science Foundation, is remarkably successful in measuring properties of the atmosphere and ionosphere with a low-cost and reliable instrument.", "",
-	12, 4,
-	"1988", "JPL submits the first GPS occultation proposal to NASA. Although the “GPS Geoscience Instrument” does not fly, the concept is established, and development of GPS remote sensing technology begins. NASA starts developing GPS remote sensing receivers, and global ground networks are put in place.", "",
-	13, 4,
-	"1980s", "As GPS emerges, NASA devises new analysis techniques and equipment. The quest for sub-centimeter global accuracy soon leads to GPS-derived measurements of the atmosphere and ionosphere.", "",
-	14, 4,
-	"1960s", "Teams from NASA JPL and Stanford University exploit radio links between Earth and the Mariner 3 and 4 spacecraft to probe the atmosphere and other properties of Mars.", "",
-	15, 4
-];
+import { DataTable } from './data.js';
+var table = DataTable;
 
 var INIT_CAMERA_Z = 8000;
 var camera, scene, renderer;
@@ -48,25 +18,40 @@ var targets = { table: [], sphere: [], helix: [], grid: [] };
 function resetCamera() {
 	camera.position.set(0, 0, INIT_CAMERA_Z); // Set position like this
 	camera.lookAt(new THREE.Vector3(0,0,10000)); // Set look at coordinate like this
-	// for (var i=0; i<objects.length; i++) {
-	// 	objects[i].rotation.x = 0;
-	// 	objects[i].rotation.y = 0;
-	// 	objects[i].rotation.z = 0;
-	// 	objects[i].quaternion.w = 1;
-	// 	objects[i].quaternion.x = 0;
-	// 	objects[i].quaternion.y = 0;
-	// 	objects[i].quaternion.z = 0;
-	// }
+	for (var i=0; i<objects.length; i++) {
+		objects[i].rotation.x = 0;
+		objects[i].rotation.y = 0;
+		objects[i].rotation.z = 0;
+		objects[i].quaternion.w = 1;
+		objects[i].quaternion.x = 0;
+		objects[i].quaternion.y = 0;
+		objects[i].quaternion.z = 0;
+	}
 }
 
 function init() {
-
-	camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
-	camera.position.z = INIT_CAMERA_Z;
-
 	document.body.addEventListener('keydown', function logKey(e) {
 	  	console.log(camera.position)
 	});
+	document.body.addEventListener('click', function down(e) {
+		var elem = e.target;
+		var parent = elem.parentElement;
+		var classes = [elem.className,parent.className,parent.parentElement.className];
+		switch(classes.indexOf('element')) {
+		  case 0:
+		    console.log(elem);
+		    break;
+		  case 1:
+		    console.log(parent);
+		    break;
+		  case 2:
+		  	console.log(parent.parentElement);
+		    break;
+		}
+	 });
+
+	camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+	camera.position.z = INIT_CAMERA_Z;
 
 	scene = new THREE.Scene();
 
@@ -92,7 +77,8 @@ function init() {
 
 		var details = document.createElement( 'div' );
 		details.className = 'details';
-		details.innerHTML = table[ i + 1 ] + '<br>' + table[ i + 2 ];
+		// var tab = '&nbsp;&nbsp;&nbsp;&nbsp;';
+		details.innerHTML = table[ i + 1 ] + '<br><img src="' + table[ i + 2 ] + '" />';
 		element.appendChild( details );
 
 		var object = new CSS3DObject( element );
@@ -109,7 +95,8 @@ function init() {
 		var xmargin = 280;
 		var xoffset = 1800;
 		var xpos = ( table[ i + 3 ] * xmargin ) - xoffset;
-		var ypos = - ( table[ i + 4 ] * 180 ) + 990;
+		var ypos = 300;
+		// var ypos = - ( table[ i + 4 ] * 180 ) + 990;
 		object.position.x = xpos * 3;
 		object.position.y = ypos * 3;
 
@@ -148,7 +135,8 @@ function init() {
 		// var tune = 0.175;
 		var tune = 0.275;
 		var theta = i * tune + Math.PI;
-		var y = - ( i * 80 ) + 450;
+		var yoffset = 800;
+		var y = - ( i * 80 ) + yoffset;
 
 		var object = new THREE.Object3D();
 
@@ -221,17 +209,15 @@ function init() {
 
 	}, false );
 
-	transform( targets.table, 2000 );
-
 	//
-
+	// set initial state
+	transform( targets.helix, 2000 );
+	//
 	window.addEventListener( 'resize', onWindowResize, false );
 
 }
 
 function transform( targets, duration ) {
-
-	resetCamera();
 
 	TWEEN.removeAll();
 
